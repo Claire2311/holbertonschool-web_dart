8266b0668da60148c979d37ebf34fdf86c53440f
Claire2311 <claire.girard@ieseg.fr>
2025-06-27 10:52:20 +0200

    task 2

--- 0x02-dart-classes/2-password.dart
+++ 0x02-dart-classes/2-password.dart
@@ -2,14 +2,12 @@ class Password {
   String password = "";
 
   bool isValid() {
-    return (password.length >= 8 && password.length <= 16) &&
-        this.password.contains(RegExp(r'[A-Z]')) &&
-        this.password.contains(RegExp(r'[a-z]')) &&
-        this.password.contains(RegExp(r'[0-9]'));
+    return ((password.length >= 8) && (password.length <= 16)) &&
+        this.password.contains(new RegExp(r"[A-Z]")) &&
+        this.password.contains(new RegExp(r"[a-z]")) &&
+        this.password.contains(new RegExp(r"[0-9]"));
   }
 
   @override
-  String toString() {
-    return "Your password is: ${this.password}";
-  }
+  String toString() => "Your Password is: ${this.password}";
 }

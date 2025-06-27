26b7e7baa4d277fe0c85d3f132f28034b2e9fd09
Claire2311 <claire.girard@ieseg.fr>
2025-06-27 10:17:04 +0200

    fix task 1

--- 0x02-dart-classes/1-usertojson.dart
+++ 0x02-dart-classes/1-usertojson.dart
@@ -5,7 +5,5 @@ class User {
 
   User({required this.name, required this.age, this.height});
 
-  String toJson() {
-    return "User(name: '${name}', age: ${age}, height: ${height})";
-  }
+  Map<String, dynamic> toJson() => {'name': name, 'age': age, 'height': height};
 }

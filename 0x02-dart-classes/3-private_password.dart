0a3b5f766de81adcdf4eb3b74768f98b0f2195b5
Claire2311 <claire.girard@ieseg.fr>
2025-06-27 11:26:34 +0200

    task 3

--- 0x02-dart-classes/3-private_password.dart
+++ 0x02-dart-classes/3-private_password.dart
@@ -2,16 +2,16 @@ class Password {
   String _password = "";
 
   Password({String password = ""}) {
-    this._password = password;
+    _password = password;
   }
 
   bool isValid() {
     return ((_password.length >= 8) && (_password.length <= 16)) &&
-        this._password.contains(new RegExp(r"[A-Z]")) &&
-        this._password.contains(new RegExp(r"[a-z]")) &&
-        this._password.contains(new RegExp(r"[0-9]"));
+        _password.contains(RegExp(r"[A-Z]")) &&
+        _password.contains(RegExp(r"[a-z]")) &&
+        _password.contains(RegExp(r"[0-9]"));
   }
 
   @override
-  String toString() => "Your Password is ${this._password}";
+  String toString() => "Your Password is ${_password}";
 }

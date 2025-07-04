93816381619f6a30c3ae27ecbf6a74f6cc016a86
Claire2311 <claire.girard@ieseg.fr>
2025-07-04 12:47:02 +0200

    Task 6

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -5,13 +5,21 @@ Future<void> printRmCharacters() async {
   try {
     final httpPackageUrl =
         Uri.parse('https://rickandmortyapi.com/api/character');
-    final httpPackageInfo = await http.read(httpPackageUrl);
-    final httpPackageJson =
-        json.decode(httpPackageInfo) as Map<String, dynamic>;
-    List results = httpPackageJson['results'];
-    for (var i = 0; i < results.length; i++) {
-      print(results[i]['name']);
+    final httpPackageInfo = await http.get(httpPackageUrl);
+    if (httpPackageInfo.statusCode == 200) {
+      final data = json.decode(httpPackageInfo.body);
+      List results = data['results'];
+      for (var i = 0; i < results.length; i++) {
+        print(results[i]['name']);
+      }
     }
+
+    // final httpPackageJson =
+    //     json.decode(httpPackageInfo) as Map<String, dynamic>;
+    // List results = httpPackageJson['results'];
+    // for (var i = 0; i < results.length; i++) {
+    //   print(results[i]['name']);
+    // }
   } catch (err) {
     print('error caught: $err');
   }

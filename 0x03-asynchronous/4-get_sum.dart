5b94eac809da72db8eb063226e98566ccc819844
Claire2311 <claire.girard@ieseg.fr>
2025-07-04 12:59:26 +0200

    Task 4

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,7 +1,7 @@
 import '4-util.dart';
 import 'dart:convert';
 
-Future<double> calculateTotal() async {
+Future<num> calculateTotal() async {
   try {
     final user = jsonDecode(await fetchUserData());
     final userOrder = jsonDecode(await fetchUserOrders(user['id']));

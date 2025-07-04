8ebed70ffee59d9a3948e4ace4b380eb17827f48
Claire2311 <claire.girard@ieseg.fr>
2025-07-04 12:36:53 +0200

    Task 5

---
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -0,0 +1,18 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+Future<void> printRmCharacters() async {
+  try {
+    final httpPackageUrl =
+        Uri.parse('https://rickandmortyapi.com/api/character');
+    final httpPackageInfo = await http.read(httpPackageUrl);
+    final httpPackageJson =
+        json.decode(httpPackageInfo) as Map<String, dynamic>;
+    List results = httpPackageJson['results'];
+    for (var i = 0; i < results.length; i++) {
+      print(results[i]['name']);
+    }
+  } catch (err) {
+    print('error caught: $err');
+  }
+}

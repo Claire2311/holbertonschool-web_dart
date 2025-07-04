d804d59fe6c58fae20fc10e63a63855b761a711d
Claire2311 <claire.girard@ieseg.fr>
2025-07-04 12:48:05 +0200

    Task 5

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -1,26 +1,52 @@
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart' as http;
+// import 'dart:convert';
+
+// Future<void> printRmCharacters() async {
+//   try {
+//     final httpPackageUrl =
+//         Uri.parse('https://rickandmortyapi.com/api/character');
+//     final httpPackageInfo = await http.get(httpPackageUrl);
+//     if (httpPackageInfo.statusCode == 200) {
+//       final data = json.decode(httpPackageInfo.body);
+//       List results = data['results'];
+//       for (var i = 0; i < results.length; i++) {
+//         print(results[i]['name']);
+//       }
+//     }
+
+//     // final httpPackageJson =
+//     //     json.decode(httpPackageInfo) as Map<String, dynamic>;
+//     // List results = httpPackageJson['results'];
+//     // for (var i = 0; i < results.length; i++) {
+//     //   print(results[i]['name']);
+//     // }
+//   } catch (err) {
+//     print('error caught: $err');
+//   }
+// }
+
 import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 Future<void> printRmCharacters() async {
   try {
-    final httpPackageUrl =
-        Uri.parse('https://rickandmortyapi.com/api/character');
-    final httpPackageInfo = await http.get(httpPackageUrl);
-    if (httpPackageInfo.statusCode == 200) {
-      final data = json.decode(httpPackageInfo.body);
-      List results = data['results'];
-      for (var i = 0; i < results.length; i++) {
-        print(results[i]['name']);
-      }
-    }
+    final response =
+        await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
 
-    // final httpPackageJson =
-    //     json.decode(httpPackageInfo) as Map<String, dynamic>;
-    // List results = httpPackageJson['results'];
-    // for (var i = 0; i < results.length; i++) {
-    //   print(results[i]['name']);
-    // }
-  } catch (err) {
-    print('error caught: $err');
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+
+      if (data['results'] is List) {
+        for (var character in data['results']) {
+          print('${character['name']}');
+        }
+      } else {
+        print('Invalid API response format.');
+      }
+    } else {
+      print('Failed to fetch data. Status code: ${response.statusCode}');
+    }
+  } catch (error) {
+    print('Error caught: $error');
   }
 }

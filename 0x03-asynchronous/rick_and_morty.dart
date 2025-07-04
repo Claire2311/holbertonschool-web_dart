0b6ab5540a8342a331a25b670164fe1ddb703e0c
Claire2311 <claire.girard@ieseg.fr>
2025-07-04 12:49:16 +0200

    Task 5

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -1,52 +1,19 @@
-// import 'package:http/http.dart' as http;
-// import 'dart:convert';
-
-// Future<void> printRmCharacters() async {
-//   try {
-//     final httpPackageUrl =
-//         Uri.parse('https://rickandmortyapi.com/api/character');
-//     final httpPackageInfo = await http.get(httpPackageUrl);
-//     if (httpPackageInfo.statusCode == 200) {
-//       final data = json.decode(httpPackageInfo.body);
-//       List results = data['results'];
-//       for (var i = 0; i < results.length; i++) {
-//         print(results[i]['name']);
-//       }
-//     }
-
-//     // final httpPackageJson =
-//     //     json.decode(httpPackageInfo) as Map<String, dynamic>;
-//     // List results = httpPackageJson['results'];
-//     // for (var i = 0; i < results.length; i++) {
-//     //   print(results[i]['name']);
-//     // }
-//   } catch (err) {
-//     print('error caught: $err');
-//   }
-// }
-
-import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 Future<void> printRmCharacters() async {
   try {
-    final response =
-        await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
+    final httpPackageUrl =
+        Uri.parse('https://rickandmortyapi.com/api/character');
+    final httpPackageInfo = await http.read(httpPackageUrl);
 
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-
-      if (data['results'] is List) {
-        for (var character in data['results']) {
-          print('${character['name']}');
-        }
-      } else {
-        print('Invalid API response format.');
-      }
-    } else {
-      print('Failed to fetch data. Status code: ${response.statusCode}');
+    final httpPackageJson =
+        json.decode(httpPackageInfo) as Map<String, dynamic>;
+    List results = httpPackageJson['results'];
+    for (var i = 0; i < results.length; i++) {
+      print(results[i]['name']);
     }
-  } catch (error) {
-    print('Error caught: $error');
+  } catch (err) {
+    print('error caught: $err');
   }
 }
